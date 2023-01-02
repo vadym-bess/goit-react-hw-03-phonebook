b4d8ec9a4aadee1a4c9904e18d774e693bf5402b
Vadym <besarabvadim2@gmail.com>
2023-01-02 20:59:32 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,23 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [ ],
     filter: '',
+  };
+
+  componentDidMount(prevProps, prevState) {
+    const storage  = localStorage.getItem('contacts');
+    const parcedContacts = JSON.parse(storage);
+   
+    if (parcedContacts) {
+       this.setState({ contacts: parcedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
   };
 
   formSubmitHandler = ({ name, number }) => {
